00260234ae0c8d9e10ccbf74dc0327da84849b7f
Priyansh Thakar <priyanshthakar@gmail.com>
2024-03-05 20:32:42 -0500

    oop complete

--- js/oop.js
+++ js/oop.js
@@ -2,15 +2,36 @@ const output = document.getElementById('output');
 const output2 = document.getElementById('output2');
 
 /* STEP 1a: Create a new object using a regular function */
-
+function createNewPerson(name) {
+    let obj = {};
+    obj.name = name;
+    obj.greetings = function () {
+        return `Hello, this is ${this.name}`;
+    };
+    return obj;
+}
 /* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+let person1 = createNewPerson("P1");
+let person2 = createNewPerson("P2");
+output.textContent = person1.greetings();
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
 rewrite the above function, without returning anything. Capitalize the name of the function. */
-
+function Person(firstName, lastName, age, gender, interests) {
+    this.name = {
+        "firstName": firstName,
+        "lastName": lastName,
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.greetings = function () {
+        return `Hello, this is ${this.name.firstName}`;
+    };
+}
 /* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
 and again invoking the .greeting() method for each person */
-
+let person3 = new Person("P3", 90, "Female", ["I1", "I2", "I3"]);
 /* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
  Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
 
@@ -22,6 +43,9 @@ and again invoking the .greeting() method for each person */
 // person1.bio()
 
 /* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
+let car = new Object();
+car.brand = "Honda";
+car.model = "Civic";
 
 /* STEP 4b: Once 'car' is created, add various properties and methods… */
 
@@ -29,6 +53,8 @@ and again invoking the .greeting() method for each person */
 
 /* STEP 5a: Yet another approach is to use the create() method. 
 Let's see how the above car object can be used to create another object */
+let anotherCar = Object.create(car);
+console.log(anotherCar.brand);
 
 /* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
 
